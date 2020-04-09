c161afdceaf806ebbab0dac2c834e041a626ad7c
winecgouvea2 <winecgouvea2@CORP.AMAZONWORKSPACES.COM>
2020-04-09 18:53:24 -0300

    Adicionar redirect para soap-to-rest

--- samples/aspnetapp/aspnetapp/Pages/api/soaptorest/Index.cshtml.cs
+++ samples/aspnetapp/aspnetapp/Pages/api/soaptorest/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace aspnetapp.Pages.api.adm
+namespace aspnetapp.Pages.api.adm.soaptorest
 {
     public class IndexModel : PageModel
     {
