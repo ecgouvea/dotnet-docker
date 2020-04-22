46d4912805eaec28d2cd12de8fa45fa75f7de916
EC2 Default User <ec2-user@ip-10-30-12-67.ec2.internal>
2020-04-22 20:55:20 +0000

    Add teste.html e script para tag e push

---
+++ samples/aspnetapp/tag-and-push.sh
@@ -0,0 +1,21 @@
+docker tag aspnetapp:latest 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-clubeextrafarma-frontend:latest
+docker push 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-clubeextrafarma-frontend:latest
+
+docker tag aspnetapp:latest 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-clubeextrafarma-backend:latest
+docker push 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-clubeextrafarma-backend:latest
+
+docker tag aspnetapp:latest 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-clubeextrafarma-adm-frontend:latest
+docker push 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-clubeextrafarma-adm-frontend:latest
+
+docker tag aspnetapp:latest 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-clubeextrafarma-adm-backend:latest
+docker push 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-clubeextrafarma-adm-backend:latest
+
+docker tag aspnetapp:latest 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-hospital-clubeextrafarma-frontend:latest
+docker push 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-hospital-clubeextrafarma-frontend:latest
+
+docker tag aspnetapp:latest 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-hospital-clubeextrafarma-backend:latest
+docker push 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-hospital-clubeextrafarma-backend:latest
+
+docker tag aspnetapp:latest 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-soaptorest-clubeextrafarma-backend:latest
+docker push 596843761193.dkr.ecr.us-east-1.amazonaws.com/fidelidade-soaptorest-clubeextrafarma-backend:latest
+
